ba53ebbc04e0f6619707e6624c0b98fa6ef695e5
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-15 12:29:55 -0500

    Update index.js
    pexels-photo-4200823.webp and title back to luke

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
      
       <main className={styles.main}>
         <h1 className={styles.title}>
-           <a href="https://en.wikipedia.org/wiki/Luke">The Book Of</a>
+          LUKE
         </h1>
          <div className={styles.hero}>
        <Image 
