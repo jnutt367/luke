3b89cce47899c09aa4b0722bfc9fa90bf25e9be5
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-14 23:02:02 -0500

    Update index.js
    Luke_1.jpg

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         </h1>
          <div className={styles.hero}>
        <Image 
-                                src="/Luke_1_833_460_80_c1.jpg"
+                                src="/Luke_1.jpg"
                                 alt="Jesus"
                                 height={900}
                                 width={2000}
