8026cc72ea6b1a71c71a14d82a4d5ee4989539fe
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-03-19 20:21:08 -0400

    Update index.js
    christian-christmas-easter-concept-birth-death-resurrection-jesus-christ-wooden-manger-nativity-scene-three-crosses-213617490.jpg

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
         </h1>
              <div  className={styles.image}>
        <Image 
-                                src="/5865-istockgetty-images-plusrudall30.jpg"
+                                src="/christian-christmas-easter-concept-birth-death-resurrection-jesus-christ-wooden-manger-nativity-scene-three-crosses-213617490.jpg"
                                 alt="Gabriel & Mary"
                                 height={900}
                                 width={2000}
