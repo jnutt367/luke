c990db457264bbc3fdc42177c4441d94f8062b1f
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-20 17:36:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
          <div className={styles.hero}>
                 <h2 className={styles.holy}>THE WORD</h2>
                 <h1 className={styles.bible}>
-                    <a href="https://the-holy-bible.vercel.app/" target="_blank">OF GOD</a>
+                    <a href="https://the-word-of-god.vercel.app/" target="_blank">OF GOD</a>
                 </h1>
         
         <h1 className={styles.title}>
