ce912e54d62a9d900ece2dfb6d855fe12e70e6c8
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-27 20:13:55 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1584,18 +1584,169 @@ The Ascension of Jesus
 
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+<main id="first_timothy" className={styles.main}>
+       
+        <div id="first_thessalonians" className={styles.hero}>
+                <h2 className={styles.holy}>THE WORD</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-word-of-god.vercel.app/" target="_blank">OF GOD</a>
+                </h1>
+        <h1 className={styles.title}>
+       TITUS
+        </h1>
+          <div className={styles.image}>
+                           <Image 
+                                                    src="/timothy.png"
+                                                    alt="hero image for Thessalonians"
+                                                    height={900}
+                                                    width={2000}
+                                                    priority
+                                                  />
+                          
+
+                        </div>
+    <p className={styles.parables}>
+
+           Titus is another book of the new testament that we wil have the description for here!
+</p>                                                      
+                  <div className={styles.nav2}>           
+      <a href="https://romans-five.vercel.app/">
+             <Image 
+                                src="/previous.webp"
+                                alt="prev"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+       </a>
+
+        
+      <a href="https://cash.app/$JNutt367">
+              <Image className={styles.arrow}
+                                src="/donations_please.webp"
+                                alt="donations_please.webp"
+                                height={75}
+                                width={90}
+                                priority
+                              /> 
+       </a>
+         <a href="https://second-corinthians.vercel.app/">
+           <Image 
+                                src="/next.webp"
+                                alt="next"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
+               <div className={styles.iframe}> 
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/PUEYCVXJM3k?autoplay=1&mute=1" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                       
+                                  </div>
+                                  
+                              
+        <div className={styles.grid2}>
+          <div className={styles.card2}>
+           <h2  className={styles.chapter2}>Titus Chapter I</h2>
+            <p> 
+
+                          
+                                        1 Paul, a servant of God and an apostle of Jesus Christ to further the faith of God’s elect and their knowledge of the truth that leads to godliness— 2 in the hope of eternal life, which God, who does not lie, promised before the beginning of time, 3 and which now at his appointed season he has brought to light through the preaching entrusted to me by the command of God our Savior,
+                                        
+                                        4 To Titus, my true son in our common faith:
+                                        
+                                        Grace and peace from God the Father and Christ Jesus our Savior.
+                                        
+                                        Appointing Elders Who Love What Is Good
+                                        5 The reason I left you in Crete was that you might put in order what was left unfinished and appoint[a] elders in every town, as I directed you. 6 An elder must be blameless, faithful to his wife, a man whose children believe[b] and are not open to the charge of being wild and disobedient. 7 Since an overseer manages God’s household, he must be blameless—not overbearing, not quick-tempered, not given to drunkenness, not violent, not pursuing dishonest gain. 8 Rather, he must be hospitable, one who loves what is good, who is self-controlled, upright, holy and disciplined. 9 He must hold firmly to the trustworthy message as it has been taught, so that he can encourage others by sound doctrine and refute those who oppose it.
+                                        
+                                        Rebuking Those Who Fail to Do Good
+                                        10 For there are many rebellious people, full of meaningless talk and deception, especially those of the circumcision group. 11 They must be silenced, because they are disrupting whole households by teaching things they ought not to teach—and that for the sake of dishonest gain. 12 One of Crete’s own prophets has said it: “Cretans are always liars, evil brutes, lazy gluttons.”[c] 13 This saying is true. Therefore rebuke them sharply, so that they will be sound in the faith 14 and will pay no attention to Jewish myths or to the merely human commands of those who reject the truth. 15 To the pure, all things are pure, but to those who are corrupted and do not believe, nothing is pure. In fact, both their minds and consciences are corrupted. 16 They claim to know God, but by their actions they deny him. They are detestable, disobedient and unfit for doing anything good.
+  </p>
+          </div>
+
+          <div className={styles.card2}>
+            <h2 className={styles.chapter2}>Titus Chapter II</h2>
+            <p> 
+                                                        Doing Good for the Sake of the Gospel
+                                    2 You, however, must teach what is appropriate to sound doctrine. 2 Teach the older men to be temperate, worthy of respect, self-controlled, and sound in faith, in love and in endurance.
+                                    
+                                    3 Likewise, teach the older women to be reverent in the way they live, not to be slanderers or addicted to much wine, but to teach what is good. 4 Then they can urge the younger women to love their husbands and children, 5 to be self-controlled and pure, to be busy at home, to be kind, and to be subject to their husbands, so that no one will malign the word of God.
+                                    
+                                    6 Similarly, encourage the young men to be self-controlled. 7 In everything set them an example by doing what is good. In your teaching show integrity, seriousness 8 and soundness of speech that cannot be condemned, so that those who oppose you may be ashamed because they have nothing bad to say about us.
+                                    
+                                    9 Teach slaves to be subject to their masters in everything, to try to please them, not to talk back to them, 10 and not to steal from them, but to show that they can be fully trusted, so that in every way they will make the teaching about God our Savior attractive.
+                                    
+                                    11 For the grace of God has appeared that offers salvation to all people. 12 It teaches us to say “No” to ungodliness and worldly passions, and to live self-controlled, upright and godly lives in this present age, 13 while we wait for the blessed hope—the appearing of the glory of our great God and Savior, Jesus Christ, 14 who gave himself for us to redeem us from all wickedness and to purify for himself a people that are his very own, eager to do what is good.
+                                    
+                                    15 These, then, are the things you should teach. Encourage and rebuke with all authority. Do not let anyone despise you.
+            </p>
+          </div>
+
+          <div
+            className={styles.card2}
+          >
+            <h2 className={styles.chapter2}>Titus Chapter III </h2>
+            <p>
+                                                                  Saved in Order to Do Good
+                                        3 Remind the people to be subject to rulers and authorities, to be obedient, to be ready to do whatever is good, 2 to slander no one, to be peaceable and considerate, and always to be gentle toward everyone.
+                                        
+                                        3 At one time we too were foolish, disobedient, deceived and enslaved by all kinds of passions and pleasures. We lived in malice and envy, being hated and hating one another. 4 But when the kindness and love of God our Savior appeared, 5 he saved us, not because of righteous things we had done, but because of his mercy. He saved us through the washing of rebirth and renewal by the Holy Spirit, 6 whom he poured out on us generously through Jesus Christ our Savior, 7 so that, having been justified by his grace, we might become heirs having the hope of eternal life. 8 This is a trustworthy saying. And I want you to stress these things, so that those who have trusted in God may be careful to devote themselves to doing what is good. These things are excellent and profitable for everyone.
+                                        
+                                        9 But avoid foolish controversies and genealogies and arguments and quarrels about the law, because these are unprofitable and useless. 10 Warn a divisive person once, and then warn them a second time. After that, have nothing to do with them. 11 You may be sure that such people are warped and sinful; they are self-condemned.
+                                        
+                                        Final Remarks
+                                        12 As soon as I send Artemas or Tychicus to you, do your best to come to me at Nicopolis, because I have decided to winter there. 13 Do everything you can to help Zenas the lawyer and Apollos on their way and see that they have everything they need. 14 Our people must learn to devote themselves to doing what is good, in order to provide for urgent needs and not live unproductive lives.
+                                        
+                                        15 Everyone with me sends you greetings. Greet those who love us in the faith.
+                                        
+                                        Grace be with you all.
+  
+                    </p>
+          </div>
+
+         
+        
+          <div className={styles.card2}
+          >
+            <h2 className={styles.chapter2}>!Explaination of Layout!</h2>
+            <p>
+            I have appended 1 Thessalonians to the bottom of 1 corinthians...
+            I have appended 2 Thessalonians to the bottom of 2 corinthians...
+            I have appended 1 Timothy to the bottom of the gospel of Matthew...
+            I will be appending 2 Timothy to the bottom of the gospel of Mark...
+            I will be appending Titus to the bottom of the gospel of Luke...
+            I will be appending Philemon to the bottom of the gospel of John...
+            I will be appending Hebrews to the bottom of the book of Acts ...
+            I will be appending James ...TBA
+            I will be appending Peter ...TBA
+            I will be appending 2 Peter ...TBA
+            I will be appending 1 John ...TBA
+            I will be appending 2 John ...TBA
+            I will be appending 3 John ...TBA
+            I will be appending Jude ...TBA
+                        More to come on the layout...!
+     </p>
+          </div>
+
+
+     
+
+        </div>
+</div>
+        <p className={styles.description}> 
+        <code className={styles.publisher}>
+        Coded, Published & Deployed by His servant: Jason Daniel Nutt 
+        </code>
+        </p>
+      </main>
+
+
+
+  
+      
     </div>
   )
 }
