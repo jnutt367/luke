62ffdc1f80bc14e3201dbad79157b23c72d97298
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-11-22 12:20:40 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,7 @@ export default function Home() {
         LUKE
         </h1>
                <div  className={styles.iframe}>
-                           <iframe width="100%" height="315" src="https://www.youtube.com/embed/i_lKfPvws_k?si=UxAbMOx2Hohytj44?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                
+                          <iframe width="100%" height="315" src="https://www.youtube.com/embed/hZVvvKbfte4?si=gxZPxuUbTB7s0hW7?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             
  
 
